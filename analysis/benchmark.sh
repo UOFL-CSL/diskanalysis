2ee351e624b0147ef0d626405df29fff52c28ce5
michael <michaelt.marzullo@gmail.com>
2019-11-25 14:20:59 -0500

    Analysis: updated the benchmarking script and started working on data collection/graphing from fio

--- analysis/benchmark.sh
+++ analysis/benchmark.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
+# Password is specified from the first argument
+
 sudo -v
 
-rm "/home/marzullo/Desktop/Stream Benchmarking/fio/trivial-readwrite-1g.0.0"
+rm "/marzunvme/Stream Benchmarking/blktrace/fio/trivial-readwrite-1g.0.0"
 
 # Start the fio benchmark with the fio_config
-gnome-terminal -- bash -c "fio fio_config --output=results.txt --output-format=json"
+screen -S fio -dm bash -c "export HISTIGNORE='*sudo -S*'; echo $1 | sudo -S -v; sudo fio fio_config --output=results.txt --output-format=json; echo Done;" &
+
+# For async io
+screen -S blktrace -dm bash -c "export HISTIGNORE='*sudo -S*'; echo $1 | sudo -S -v; sudo ./blktrace -d /dev/nvme0n1 -P 0 -o - | sudo ./blkparse -i -; echo Done; exec bash" &
 
 # Poll for the started fio trace
-loops=100
+loops=10
 pid=0
 
 while [ $loops -gt 0 ]; do
     sleep 0.1
     loops=$(($loops-1))
-    pid=$(ps aux | grep fio | grep -v grep | awk '{print $2}')
+    pids=$(ps -T aux | grep fio | grep -v grep | grep -v bash)
 
-    if [ $pid -gt 0 ]
+    count=$(echo "$pids" | wc -l)
+
+    echo $count
+
+    if [ $count -gt 2 ]
         then
         loops=0
+        pid=$(echo "$pids" | awk 'END{print $3}')
     fi    
 done
 
 echo Found pid $pid
 
 # Start blktrace
-sudo gnome-terminal -- bash -c "./blktrace -d /dev/sda -P $pid -o - | ./blkparse -i -"
+#screen -S blktrace -dm bash -c "export HISTIGNORE='*sudo -S*'; echo $1 | sudo -S -v; sudo ./blktrace -d /dev/nvme0n1 -P $pid -o - | sudo ./blkparse -i -; echo Done; exec bash" &
 
 # Start algorithm
-sudo gnome-terminal -- bash -c "python3 ~/Desktop/DiskAnalysis/analysis/marzullo.py"
+#sudo gnome-terminal -- bash -c "python3 ~/Desktop/DiskAnalysis/analysis/marzullo.py"
 
--- analysis/datacollection.py
+++ analysis/datacollection.py
@@ -2,8 +2,27 @@
 import json
 import matplotlib as plt
 
-output = open("results.txt")
+# Contains the fio result outputs
+experimentData = {}
+experimentData["iops"] = []
+experimentData["write_bw"] = []
+experimentData["runtime"] = []
 
-parsed = json.load(output)
+# LBAs that are repeated in the experiment
+experimentData["lbas"] = []
 
-print(parsed["jobs"][0]["write"]["runtime"])
+# Output of the algorithm
+algorithmOutput = {}
+
+for i in range(1,11):
+    output = open("results_" + str(i) + ".txt")
+    parsed = json.load(output)
+
+    writejob = parsed["jobs"][0]["write"]
+    iops = writejob["iops"]
+    runtime = writejob["runtime"]
+    bw = writejob["bw"]
+    
+    experimentData["runtime"].append(runtime)
+    experimentData["write_bw"].append(bw)
+    experimentData["iops"].append(iops)
